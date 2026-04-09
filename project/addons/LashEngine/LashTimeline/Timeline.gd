62b3b166df02fd6bccae684d9f5f5ea4ce23d7cd
celyk <humanimations10@gmail.com>
2026-04-09 16:51:11 +1000

    Begin timeline interface

---
+++ project/addons/LashEngine/LashTimeline/Timeline.gd
@@ -0,0 +1,62 @@
+@tool
+class_name LashTimeline extends Control
+
+var state := TimelineState.new()
+
+func play() -> void:
+	pass
+
+## Scrubs to the frame in the timeline.
+func seek_frame(frame:int) -> void:
+	state.frame = frame
+
+## Scrubs to the keyframe in the current layer.
+func seek_keyframe(keyframe:int) -> void:
+	pass
+
+func _gui_input(event: InputEvent) -> void:
+	print(event)
+	if event is InputEventMouseButton:
+		var frame_id := _get_frame_id(event.position)
+		seek_frame(frame_id.x)
+	if event is InputEventMouseMotion:
+		if event.button_mask == MouseButtonMask.MOUSE_BUTTON_MASK_LEFT:
+			var frame_id := _get_frame_id(event.position)
+			seek_frame(frame_id.x)
+
+func _process(delta: float) -> void:
+	queue_redraw()
+
+func _draw() -> void:
+	#draw_set_transform(Vector2(), 0.0, Vector2.ONE)
+	
+	draw_rect(Rect2(Vector2(), size), Color.WHITE.darkened(0.7))
+	
+	var frame_scale : Vector2 = _get_frame_size()
+	var frame_size : Vector2 = _get_frame_size()
+	
+	var padding := Vector2(2, 2)
+	frame_size -= padding
+	
+	#draw_set_transform(Vector2(), 0.0, frame_scale)
+	
+	for j in range(0, state.zoom.y):
+		for i in range(0, state.zoom.x):
+			var frame_pos := Vector2(i, j) * frame_scale
+			var color := Color.WHITE.darkened(0.5)
+			
+			#frame_pos += padding / 2
+			
+			draw_rect(Rect2(frame_pos, frame_size), color, true, -1.0, true)
+			draw_circle(frame_pos + frame_size / 2, frame_size.x / 2 / 5, Color.BLACK, true, -1.0, true)
+	
+	var needle_x : float = state.frame * frame_scale.x + 0.5 * frame_size.x
+	
+	if 0.0 <= needle_x and needle_x <= size.x:
+		draw_line(Vector2(needle_x, 0), Vector2(needle_x, size.y), Color.RED.darkened(0.3), 1.0, true)
+
+func _get_frame_size() -> Vector2:
+	return size / state.zoom
+
+func _get_frame_id(pos:Vector2) -> Vector2i:
+	return Vector2i(pos / _get_frame_size())
---
+++ project/addons/LashEngine/LashTimeline/TimelineState.gd
@@ -0,0 +1,6 @@
+class_name TimelineState extends Resource
+
+var frame := 0
+var layer := 0
+
+var zoom : Vector2 = Vector2(60, 4)
